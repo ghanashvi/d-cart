d5360495318c3050bc89681bd1e6dea6a7eb7ef2
ghanashvi <ghanashviparihar@gmail.com>
2023-06-02 00:52:38 +0530

    first commit

---
+++ scripts/deploy.js
@@ -0,0 +1,49 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+const hre = require("hardhat")
+const { items } = require("../src/items.json")
+
+const tokens = (n) => {
+  return ethers.utils.parseUnits(n.toString(), 'ether')
+}
+
+async function main() {
+  // Setup accounts
+  const [deployer] = await ethers.getSigners()
+
+  // Deploy Dappazon
+  const D_cart = await hre.ethers.getContractFactory("D_cart")
+  const d_cart = await D_cart.deploy()
+  await d_cart.deployed()
+
+  console.log(`Deployed D_cart Contract at: ${d_cart.address}\n`)
+
+  // Listing items...
+  for (let i = 0; i < items.length; i++) {
+    const transaction = await d_cart.connect(deployer).list(
+      items[i].id,
+      items[i].name,
+      items[i].category,
+      items[i].image,
+      tokens(items[i].price),
+      items[i].rating,
+      items[i].stock,
+    )
+
+    await transaction.wait()
+
+    console.log(`Listed item ${items[i].id}: ${items[i].name}`)
+  }
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
+
